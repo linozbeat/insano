67184a548ca6e9998b5b449fc1ec25a02ad4e928
linozbeat <143718060+linozbeat@users.noreply.github.com>
2025-02-06 06:42:33 -0300

    Update funcoes.js

--- js/funcoes.js
+++ js/funcoes.js
@@ -2,14 +2,5 @@
 
 
 async function criarConta(){
-    nome = await $tbNome.value
-    sobrenome = await $tbSobrenome.value
-    idade = await $tbIdade.value
-    var nomeCompleto = nome + " " + sobrenome
-    
-   console.log("ola " + nome + " " + sobrenome + " sua idade é " + idade)
-   var dados = nomeCompleto + " " + idade 
-    console.log(nome)
-   console.log(dados)
- 
+
 }
